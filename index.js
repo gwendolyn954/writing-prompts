30ca0227a6b10d95bb39a7ac6698120d6311a542
Gwendolyn Asani <107776357+gwendolyn954@users.noreply.github.com>
2023-07-23 22:05:03 -0400

    removed firebase comments

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//Firebase set up:
-
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
